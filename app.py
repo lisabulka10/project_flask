3ee32bd6575d92a7fa70b8cdf3668f0d287fc275
lisabulka10 <es.balykova@gmail.com>
2025-06-14 19:47:57 +0300

    first change

--- app.py
+++ app.py
@@ -3,10 +3,45 @@ from flask import Flask
 # Создаем объект приложения Flask
 app = Flask(__name__)
 
-# Определяем маршрут и привязываем его к функции
-@app.route('/')
+
+@app.route('/hello')
 def hello():
-    return "Hello, Flask!"
+    return 'Hello, world!'
+
+
+@app.route('/info')
+def info():
+    return "This is an informational page."
+
+
+@app.route('/calc/<num1>/<num2>')
+def calc(num1, num2):
+    try:
+        sum_num = int(num1) + int(num2)
+        return f'The sum of {num1} and {num2} is {sum_num}'
+    except ValueError:
+        return f'The {num1} and {num2} is not a number.'
+
+
+@app.route('/reverse/<text>')
+def reverse(text):
+    try:
+        return text[::-1]
+    except ValueError:
+        return 'This is a mistake in your text.'
+
+
+@app.route('/user/<name>/<age>')
+def hello_user(name, age):
+    try:
+        name = str(name)
+        age = int(age)
+        if age < 0 or len(name) < 1 or name.isnumeric():
+            raise ValueError
+        return f'Hello, {name}! You are {age} years old.'
+    except ValueError:
+        return 'Valid name or age'
+
 
 # Запуск приложения
 if __name__ == "__main__":
