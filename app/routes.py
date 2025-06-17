53d374f37dd5d1c1b0d6cbb8d0a3bded77420b6b
lisabulka10 <es.balykova@gmail.com>
2025-06-17 12:29:30 +0300

    lesson 5

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,20 @@
+from flask import render_template
 from app import app
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 @app.route('/hello')
